e8c9d2d4ceb50557f70059fd2333a17c820b12f0
dimentio233 <johnnyhe7@outlook.com>
2025-06-24 11:28:13 -0700

    changed permission.

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
+var Version = "dev"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"github.com/go-johnnyhe/leetmock/cmd"
 )	
 
+var version = "dev"
+
 func main() {
+	cmd.Version = version
 	cmd.Execute()
 }
