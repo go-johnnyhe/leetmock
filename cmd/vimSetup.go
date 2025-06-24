f550cf390b21582245b2f421407c93181ecf43c5
dimentio233 <johnnyhe7@outlook.com>
2025-06-23 17:20:32 -0700

    revised luasnippet.

--- cmd/vimSetup.go
+++ cmd/vimSetup.go
@@ -17,12 +17,22 @@ import (
 //go:embed extras/autoread.vim
 var pluginBody []byte
 
-const luaSnippet = `-- after/plugin/leetmock.lua
+const luaSnippet = `-- ~/.config/nvim/after/plugin/leetmock.lua
 vim.opt.autoread = true
 vim.opt.updatetime = 500
+local group = vim.api.nvim_create_augroup("leetmock_autoread", { clear = true })
+
 vim.api.nvim_create_autocmd(
-	{"FocusGained", "BufEnter", "CursorHold", "CursorHoldI"},
-	{ pattern = "*", command = "checktime" }
+  { "FocusGained", "BufEnter", "CursorHold", "CursorHoldI", "TermEnter" },
+  {
+    group = group,
+    pattern = "*",
+    callback = function()
+      -- pcall avoids 'checktime' errors in special buffers
+      pcall(vim.cmd, "checktime")
+    end,
+    desc = "Reload buffer if the file changed on disk",
+  }
 )`
 
 var vimSetupCmd = &cobra.Command{
